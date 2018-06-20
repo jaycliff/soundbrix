26a889cce1134097eaa7af633d3498ac8c964d5a
Cliffmeister <jaycliff702@gmail.com>
2018-06-20 18:01:45 +0800

    UPDATE
    Various improvements and fixes

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 soundbrix
 =========
 
-a small and simple sound library for creating playable sound objects. Powered by HTML5 Web Audio API.
--- soundbrix_v3.js
+++ soundbrix_v3.js
@@ -17,11 +17,15 @@
 /*
     SoundBrix: Experimental HTML5 Sound Object Creation Library powered by Web Audio API (Version 1.3 - 06/30/2014)
     Author: Jaycliff Arcilla
+
+    NOTE(S):
+        - AudioScheduledSourceNode.onended fires when it has literally finished the playback, yes that means even when deliberately stopped via the stop() method
+        - create silence -> SOUNDBRIX.ctx.createBuffer(2, SOUNDBRIX.ctx.sampleRate * sound.audioBuffer().duration, SOUNDBRIX.ctx.sampleRate)
 */
 /*global AudioContext, AudioBuffer*/
 /*jslint browser: true, devel: true, nomen: true, unparam: true, sub: true, regexp: true */
 
-var SOUNDBRIX = (function (AudioContext) {
+var SOUNDBRIX = (function (AudioContext, max) {
     "use strict";
     var ctx = new AudioContext(),
         masterGain = ctx.createGain(),
@@ -31,7 +35,7 @@ var SOUNDBRIX = (function (AudioContext) {
     masterGain.connect(ctx.destination);
     function getSettings(us) {
         var key, ds = {
-            html5: false,
+            type: 'multishot',
             playbackRate: 1,
             volume: 1
         };
@@ -54,16 +58,18 @@ var SOUNDBRIX = (function (AudioContext) {
         };
         request.send();
     }
+    function toString() {
+        return 'function ' + this.name + '() { [native code] }';
+    }
     function Multishot(settings) {
         var that = this,
             playing = false,
             vol = settings.volume,
             playbackRate = settings.playbackRate,
-            audio_buffer = null,
+            internalAudioBuffer = settings.audioBuffer instanceof AudioBuffer ? settings.audioBuffer : null,
             gainNode = ctx.createGain(),
-            internal_list_of_bsn = [],
-            source_url = settings.src || settings.href || settings.source || settings.url || settings.location;
-        console.log(source_url, settings);
+            internalListOfBSN = [],
+            sourceURL = settings.src || settings.href || settings.source || settings.url || settings.location;
         gainNode.connect(masterGain);
         this.getGainNode = function getGainNode() {
             return gainNode;
@@ -72,17 +78,17 @@ var SOUNDBRIX = (function (AudioContext) {
             return settings.type;
         };
         this.getDuration = function getDuration() {
-            if (audio_buffer) {
-                return audio_buffer.duration;
+            if (internalAudioBuffer) {
+                return internalAudioBuffer.duration;
             }
             return 0;
         };
         this.audioBuffer = function audioBuffer(ab) {
             if (arguments.length) {
-                audio_buffer = ab;
+                internalAudioBuffer = ab;
                 return that;
             }
-            return audio_buffer;
+            return internalAudioBuffer;
         };
         this.volume = function volume(value) {
             if (typeof value !== "number") {
@@ -95,82 +101,85 @@ var SOUNDBRIX = (function (AudioContext) {
             return vol;
         };
         this.isReady = function isReady() {
-            return !!audio_buffer;
+            return !!internalAudioBuffer;
         };
         this.isPlaying = function isPlaying() {
             return playing;
         };
         function onended() {
-            internal_list_of_bsn.shift();
-            if (internal_list_of_bsn.length === 0) {
+            internalListOfBSN.shift();
+            if (internalListOfBSN.length === 0) {
                 playing = false;
-                (settings.onend || noop)();
+                (settings.onend || noop).call(this);
             }
-            //console.log(this === internal_list_of_bsn.shift());
+            //console.log(this === internalListOfBSN.shift());
         }
         this.play = function play() {
             var bsNode = ctx.createBufferSource(), currentTime = ctx.currentTime;
-            if (audio_buffer) {
-                bsNode.buffer = audio_buffer;
+            if (internalAudioBuffer) {
+                bsNode.buffer = internalAudioBuffer;
                 //bsNode.loop = settings.loop;
                 //bsNode.detune.setValueAtTime(-1200, ctx.currentTime);
                 bsNode.playbackRate.setValueAtTime(playbackRate, currentTime);
                 bsNode.onended = onended;
                 bsNode.connect(gainNode);
                 bsNode.start(currentTime);
-                internal_list_of_bsn.push(bsNode);
+                internalListOfBSN.push(bsNode);
                 if (!playing) {
                     playing = true;
-                    (settings.onplay || noop)();
+                    (settings.onplay || noop).call(this);
                 }
             }
             return that;
         };
         this.stop = function stop() {
             var k, len, bsn;
-            if (audio_buffer && playing) {
+            if (internalAudioBuffer && playing) {
                 playing = false;
-                for (k = 0, len = internal_list_of_bsn.length; k < len; k += 1) {
-                    bsn = internal_list_of_bsn[k];
+                for (k = 0, len = internalListOfBSN.length; k < len; k += 1) {
+                    bsn = internalListOfBSN[k];
                     bsn.onended = null;
                     bsn.stop();
                 }
-                internal_list_of_bsn.length = 0;
-                (settings.onstop || noop)();
+                internalListOfBSN.length = 0;
+                (settings.onstop || noop).call(this);
             }
             return that;
         };
-        loadSound(
-            source_url,
-            function (response) {
-                ctx.decodeAudioData(
-                    response,
-                    function success(buffer) {
-                        audio_buffer = buffer;
-                        if (typeof settings.onload === "function") {
-                            setTimeout(settings.onload, 0);
+        if (!internalAudioBuffer) {
+            loadSound(
+                sourceURL,
+                function (response) {
+                    ctx.decodeAudioData(
+                        response,
+                        function success(buffer) {
+                            internalAudioBuffer = buffer;
+                            setTimeout((settings.onload || noop), 0);
+                        },
+                        function fail() {
+                            throw new Error('Failed to load source from ' + sourceURL);
                         }
-                    },
-                    function fail() {
-                        throw new Error('Failed to load source from ' + source_url);
-                    }
-                );
-            }
-        );
+                    );
+                }
+            );
+        } else {
+            setTimeout((settings.onload || noop), 0);
+        }
     }
     function Loop(settings) {
         var that = this,
             playing = false,
             breakLoop = false,
+            playTimerID = 0,
+            isPlayTimerSet = false,
             vol = settings.volume,
             playbackRate = settings.playbackRate,
-            audio_buffer = settings.audioBuffer instanceof AudioBuffer ? settings.audioBuffer : null,
+            internalAudioBuffer = settings.audioBuffer instanceof AudioBuffer ? settings.audioBuffer : null,
             gainNode = ctx.createGain(),
             currentBSN = null,
             nextBSN = null,
             nextPlaybackTime = 0,
-            source_url = settings.src || settings.href || settings.source || settings.url || settings.location;
-        console.log(source_url, settings);
+            sourceURL = settings.src || settings.href || settings.source || settings.url || settings.location;
         gainNode.connect(masterGain);
         this.getGainNode = function getGainNode() {
             return gainNode;
@@ -179,17 +188,17 @@ var SOUNDBRIX = (function (AudioContext) {
             return settings.type;
         };
         this.getDuration = function getDuration() {
-            if (audio_buffer) {
-                return audio_buffer.duration;
+            if (internalAudioBuffer) {
+                return internalAudioBuffer.duration;
             }
             return 0;
         };
         this.audioBuffer = function audioBuffer(ab) {
             if (arguments.length) {
-                audio_buffer = ab;
+                internalAudioBuffer = ab;
                 return that;
             }
-            return audio_buffer;
+            return internalAudioBuffer;
         };
         this.volume = function volume(value) {
             if (typeof value !== "number") {
@@ -202,58 +211,80 @@ var SOUNDBRIX = (function (AudioContext) {
             return vol;
         };
         this.isReady = function isReady() {
-            return !!audio_buffer;
+            return !!internalAudioBuffer;
         };
         this.isPlaying = function isPlaying() {
             return playing;
         };
+        this.isWaiting = function isWaiting() {
+            return isPlayTimerSet;
+        };
         function onended() {
             if (!breakLoop) {
-                nextPlaybackTime += audio_buffer.duration;
-                console.log(nextPlaybackTime);
+                nextPlaybackTime += internalAudioBuffer.duration;
                 currentBSN = nextBSN;
                 nextBSN = ctx.createBufferSource();
-                nextBSN.buffer = audio_buffer;
+                nextBSN.buffer = internalAudioBuffer;
                 nextBSN.playbackRate.setValueAtTime(playbackRate, ctx.currentTime);
                 nextBSN.onended = onended;
                 nextBSN.connect(gainNode);
                 nextBSN.start(nextPlaybackTime);
-                (settings.onloop || noop)();
+                (settings.onloop || noop).call(this, nextPlaybackTime);
             } else {
                 breakLoop = false;
                 nextPlaybackTime = 0;
                 playing = false;
                 currentBSN = null;
-                (settings.onstop || noop)();
+                (settings.onstop || noop).call(this);
             }
         }
         this.nextLoopTime = function nextLoopTime() {
             return nextPlaybackTime;
         };
-        this.play = function play() {
-            var currentTime = ctx.currentTime;
-            if (audio_buffer && !playing) {
-                playing = true;
-                nextPlaybackTime = currentTime + audio_buffer.duration;
+        function playEventTimerCallback() {
+            playing = true;
+            isPlayTimerSet = false;
+            (settings.onplay || noop).call(this, nextPlaybackTime);
+        }
+        this.play = function play(specificTime) {
+            var currentTime = ctx.currentTime, startTime = 0, timeout = 0;
+            if (arguments.length > 0) {
+                if (typeof specificTime !== "number") {
+                    throw new TypeError('parameter must be a number');
+                }
+            } else {
+                specificTime = 0;
+            }
+            if (internalAudioBuffer && !playing && !isPlayTimerSet) {
+                startTime = max(specificTime, currentTime);
+                nextPlaybackTime = startTime + internalAudioBuffer.duration;
                 currentBSN = ctx.createBufferSource();
-                currentBSN.buffer = audio_buffer;
-                currentBSN.playbackRate.setValueAtTime(playbackRate, currentTime);
+                currentBSN.buffer = internalAudioBuffer;
+                currentBSN.playbackRate.setValueAtTime(playbackRate, startTime);
                 currentBSN.onended = onended;
                 currentBSN.connect(gainNode);
-                currentBSN.start(currentTime);
+                currentBSN.start(startTime);
                 nextBSN = ctx.createBufferSource();
-                nextBSN.buffer = audio_buffer;
-                nextBSN.playbackRate.setValueAtTime(playbackRate, currentTime);
+                nextBSN.buffer = internalAudioBuffer;
+                nextBSN.playbackRate.setValueAtTime(playbackRate, startTime);
                 nextBSN.onended = onended;
                 nextBSN.connect(gainNode);
                 nextBSN.start(nextPlaybackTime);
-                (settings.onplay || noop)();
+                timeout = max(0, (specificTime - currentTime) * 1000);
+                if (timeout === 0) {
+                    playing = true;
+                    (settings.onplay || noop).call(this, nextPlaybackTime);
+                } else {
+                    playTimerID = setTimeout(playEventTimerCallback, timeout);
+                    isPlayTimerSet = true;
+                    (settings.onwait || noop).call(this, startTime);
+                }
             }
             return that;
         };
         // 'end' basically stops the loop, but waits for the current sound to finish playing
         this.end = function end() {
-            if (audio_buffer && playing) {
+            if (internalAudioBuffer && playing || isPlayTimerSet) {
                 breakLoop = true;
                 nextBSN.onended = null;
                 nextBSN.stop(0);
@@ -261,45 +292,62 @@ var SOUNDBRIX = (function (AudioContext) {
             }
             return that;
         };
-        this.stop = function stop() {
-            if (audio_buffer && playing) {
-                playing = false;
-                nextPlaybackTime = 0;
-                currentBSN.onended = null;
+        function resetBSNs() {
+            nextPlaybackTime = 0;
+            currentBSN.onended = null;
+            currentBSN.stop(0);
+            currentBSN = null;
+            if (!breakLoop) {
                 nextBSN.onended = null;
-                currentBSN.stop(0);
                 nextBSN.stop(0);
-                currentBSN = null;
                 nextBSN = null;
-                (settings.onstop || noop)();
+            } else {
+                breakLoop = false;
+            }
+        }
+        this.stop = function stop() {
+            if (internalAudioBuffer && playing) {
+                playing = false;
+                resetBSNs();
+                (settings.onstop || noop).call(this);
+            } else if (isPlayTimerSet) {
+                clearTimeout(playTimerID);
+                isPlayTimerSet = false;
+                resetBSNs();
+                (settings.onwaitcancel || noop).call(this);
             }
             return that;
         };
-        loadSound(
-            source_url,
-            function (response) {
-                ctx.decodeAudioData(
-                    response,
-                    function success(buffer) {
-                        audio_buffer = buffer;
-                        if (typeof settings.onload === "function") {
-                            setTimeout(settings.onload, 0);
+        if (!internalAudioBuffer) {
+            loadSound(
+                sourceURL,
+                function (response) {
+                    ctx.decodeAudioData(
+                        response,
+                        function success(buffer) {
+                            internalAudioBuffer = buffer;
+                            setTimeout((settings.onload || noop), 0);
+                        },
+                        function fail() {
+                            throw new Error('Failed to load source from ' + sourceURL);
                         }
-                    },
-                    function fail() {
-                        throw new Error('Failed to load source from ' + source_url);
-                    }
-                );
-            }
-        );
+                    );
+                }
+            );
+        } else {
+            setTimeout((settings.onload || noop), 0);
+        }
     }
+    Multishot.toString = toString;
+    Loop.toString = toString;
+    toString.toString = toString;
     return {
-        createSound: function createSound(user_settings) {
+        createSound: function createSound(userSettings) {
             var settings;
-            if (typeof user_settings !== "object") {
+            if (typeof userSettings !== "object") {
                 throw new TypeError('parameter must be a plain object');
             }
-            settings = getSettings(user_settings);
+            settings = getSettings(userSettings);
             switch (settings.type) {
             case 'multishot':
                 return new Multishot(settings);
@@ -313,4 +361,4 @@ var SOUNDBRIX = (function (AudioContext) {
         context: ctx,
         masterGain: masterGain
     };
-}((typeof AudioContext === "function" && AudioContext) || window.AudioContext || window.webkitAudioContext));
+}((typeof AudioContext === "function" && AudioContext) || window.AudioContext || window.webkitAudioContext, Math.max));
